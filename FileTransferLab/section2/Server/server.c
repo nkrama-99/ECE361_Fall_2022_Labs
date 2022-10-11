91a6a4b2e852c7372f23cc7e563a91930a9243c6
Gaurav Ranganath <gauravranganath@Gauravs-MBP.ht.home>
2022-10-11 00:31:40 -0400

    fixed server and client

--- FileTransferLab/section2/Server/server.c
+++ FileTransferLab/section2/Server/server.c
@@ -1,142 +1,231 @@
-// we used the beej textbook for reference
+/* Some code is borrowed from Section 2.4 of the Berkeley API */
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <string.h>
 #include <netinet/in.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <time.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define SERVER_UDP_PORT 5000 /* well-known port */
+#define MAXLEN 4096          /* maximum data length */
+
+void write_to_file(char *received);
+
+void delay(int number_of_seconds) 
+{ 
+    // Converting time into milli_seconds 
+    int milli_seconds = 1000 * number_of_seconds; 
+  
+    // Storing start time 
+    clock_t start_time = clock(); 
+  
+    // looping till required time is not achieved 
+    while (clock() < start_time + milli_seconds) {
+        // printf("waiting\n");
+    };
+
+} 
+
+void write_to_file(char *received)
+{    
+    char *total_frag = (char*)malloc(MAXLEN * sizeof(char));
+    char *packet_no = (char*)malloc(MAXLEN * sizeof(char));
+    char *size = (char*)malloc(MAXLEN * sizeof(char));
+    char *file_name = (char*)malloc(MAXLEN * sizeof(char));   
+    
+    char *content_ptr;
+
+    // printf("Processing total_frag\n");
+    int index = 0;
+    int counter = 0;
+
+    while (*(received + index) != ':') {
+        // printf("Testing with: %c\n", *(received + index));
+        *(total_frag + counter) = *(received + index);
+        counter++;
+        index++;
+    };
+    *(total_frag + counter) = '\0';
+    index++;
+    
+    // printf("Processing packet_no\n");
+    counter = 0;
+    while (*(received + index) != ':') {
+        // printf("Testing with: %c\n", *(received + index));
+        *(packet_no + counter) = *(received + index);
+        counter++;
+        index++;
+    };
+    *(packet_no + counter) = '\0';
+    index++;
+
+    // printf("Processing size\n");
+    counter = 0;
+    while (*(received + index) != ':') {
+        // printf("Testing with: %c\n", *(received + index));
+        *(size + counter) = *(received + index);
+        counter++;
+        index++;
+    };
+    *(size + counter) = '\0';
+    index++;
+
+    // printf("Processing file_name\n");
+    counter = 0;
+    while (*(received + index) != ':') {
+        // printf("Testing with: %c\n", *(received + index));
+        *(file_name + counter) = *(received + index);
+        counter++;
+        index++;
+    };
+    *(file_name + counter) = '\0';
+    index++;
+
+    content_ptr = received + index;
+
+    int inc_limit = atoi(size);
+    char *content = (char*)malloc(inc_limit * sizeof(char));
+    
+    for (int i=0; i<inc_limit; i++) {
+        content[i] = *(content_ptr + i);
+    }
+
+    // printf("Parsing the message received:\n");
+    // printf(" - total_frag : %s \n", total_frag);
+    // printf(" - packet_no : %s \n", packet_no);
+    // printf(" - size : %s \n", size);
+    // printf(" - file_name : %s \n", file_name);
+    // // printf(" - content : %s \n", content);
+
+    printf("Writing to file | total_frag = %s | packet_no = %s | size = %s | file_name = %s \n", total_frag , packet_no, size, file_name);
+
+    // testing last packet for a specific case
+    // if (atoi(packet_no) == 2168) {
+    //     printf("\n");
+    //     printf(content);
+    //     printf("\n");
+    // }
+    
+    FILE *fptr;
+
+    if (atoi(packet_no) == 1)
+    {
+        fptr = fopen(file_name, "w+b"); // "w" defines "writing mode"
+    }
+    else
+    {
+        fptr = fopen(file_name, "ab"); // "w" defines "writing mode"
+    }
+    fflush(fptr);
+
+    char c = *content;
+    int inc = 0;
+
+    fwrite(content, 1, atoi(size), fptr);
+
+    fclose(fptr);
+
+    // free up all memory allocated
+    free(total_frag);
+    free(packet_no);
+    free(size);
+    free(file_name);
+    free(content);
+}
 
 int main(int argc, char **argv)
 {
-    char ftp[100];
-    char fileName[100];
+    //variables borrowed from Berkely API 2.4
+    int sd, client_len, port, n;
+    char buf[MAXLEN];
+    struct sockaddr_in server, client;
+    char expected_message[] = "ftp";
+    char *reply;
+    int reply_len;
 
-    if (argc != 3)
+    reply = malloc(3 * sizeof(char));
+    reply = "yes";
+    reply_len = 3;
+
+    switch (argc)
     {
-        printf("error: bad args\n");
+    case 2:
+        port = atoi(argv[1]);
+        break;
+    default:
+        fprintf(stderr, "Usage: %s <UDP listen port>\n", argv[0]);
         exit(1);
     }
 
-    printf("Please input message as follows: ftp <filename>\n");
-    scanf("%s%s", ftp, fileName);
+    //the following is borrowed from the Berkley API
+    //establishes socket and binds to it
 
-    int status;
-    struct addrinfo hints;
-    struct addrinfo *servinfo;
+    //AF_INET family: internet communication using TCP/IP
+    //SOCK_DRAM type: delivers data in blocks of bytes
+    //0 protocol: default protocol for family, type pair
+    //the default protocol for AF_INET,SOCK_DGRAM is UDP
+    //sd is the socket descriptor returned
 
-    memset(&hints, 0, sizeof(hints));
-
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_protocol = IPPROTO_UDP;
-
-    status = getaddrinfo(argv[1], argv[2], &hints, &servinfo);
-
-    int sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-    if (sockfd < 0)
+    //----------------------------------------------------------------
+    if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
     {
-        printf("error: socket");
+        fprintf(stderr, "Can’t create a socket\n");
+        exit(1);
+    }
+    /* Bind an address to the socket */
+    //erase data @ address of server
+    bzero((char *)&server, sizeof(server));
+
+    server.sin_family = AF_INET;
+    //convert host byte order to network byte order (Internet)
+    //to use a common data format
+    server.sin_port = htons(port);
+    //use any ip address available for host
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    //communication will occur via the specified port number
+    if (bind(sd, (struct sockaddr *)&server,
+             sizeof(server)) == -1)
+    {
+        fprintf(stderr, "Can’t bind name to socket\n");
         exit(1);
     }
 
-    clock_t start;
-    clock_t end;
-    double rtt;
-
-    start = clock();
-
-    if (access(fileName, F_OK) == 0)
+    //-----------------------------------------------------------------------
+    //wait for an incoming message
+    while (1)
     {
-        if (sendto(sockfd, ftp, sizeof(ftp), 0, servinfo->ai_addr, servinfo->ai_addrlen) < 0)
+        client_len = sizeof(client);
+        //receive message from client into buf
+        //returns number of bytes received
+        if ((n = recvfrom(sd, buf, MAXLEN, 0,
+                          (struct sockaddr *)&client, &client_len)) < 0)
         {
-            printf("error: sendto\n");
+            fprintf(stderr, "Can’t receive datagram\n");
             exit(1);
         }
-        printf("Sending to server...\n");
-    }
-    else
-    {
-        printf("error: access\n");
-        exit(1);
-    }
 
-    char buff[100];
-    socklen_t serversize = sizeof(servinfo);
+        // delay(500);
+        // printf("PRINTING BUF RECEIVED BY UDP FUNCTION\n");
+        // printf(buf);
+        // printf("\n End of buf \n");
+        write_to_file(buf);
 
-    int recv = recvfrom(sockfd, buff, sizeof(buff), 0, (struct sockaddr *)servinfo, &serversize);
-    if (recv < 0)
-    {
-        printf("error: recvfrom\n");
-        exit(1);
-    }
-
-    buff[recv] = '\0';
-
-    if (strcmp(buff, "yes") == 0)
-    {
-        end = clock();
-        rtt = ((double)(end - start)) / CLOCKS_PER_SEC;
-        printf("A file transfer can start.\n");
-        printf("Round-trip time from client to server is %f seconds.\n", rtt);
-    }
-    else if (strcmp(buff, "no") == 0)
-    {
-        printf("A file transfer cannot start.\n");
-        exit(1);
-    }
-    else
-    {
-        printf("Other, invalid response\n");
-    }
-
-    FILE *file;
-    char file_buf[1000];
-    char pack_buf[4096];
-
-    file = fopen(fileName, "r");
-    int file_size = ftell(file);
-
-    if (file == NULL)
-    {
-        printf("error: empty file\n");
-    }
-    else
-    {
-        printf("file is not empty\n");
-    }
-
-    fseek(file, 0, SEEK_END);
-    int total_frag = (ftell(file) / 1000) + 1;
-    rewind(file);
-
-    for (int frag_num = 1; frag_num <= total_frag; frag_num++)
-    {
-        int size = fread(file_buf, sizeof(char), 1000, file);
-        printf("%s\n", file_buf);
-        int header_offset = sprintf(pack_buf, "%d:%d:%d:%s:", total_frag, frag_num, size, fileName);
-        memcpy(pack_buf + header_offset, file_buf, size);
-
-        if (sendto(sockfd, pack_buf, header_offset + size, 0, servinfo->ai_addr, servinfo->ai_addrlen) < 0)
+        //define reply and reply_len
+        if (sendto(sd, reply, reply_len, 0,
+                   (struct sockaddr *)&client, client_len) != reply_len)
         {
-            printf("error: sendto");
+            fprintf(stderr, "Can’t send datagram\n");
+            exit(1);
         }
 
-        if (recvfrom(sockfd, buff, 256, 0, (struct sockaddr *)servinfo, &serversize) < 0)
-        {
-            printf("error: recvfrom");
-        }
-
-        if (strcmp(buff, "yes") == 0)
-        {
-            printf("ACK\n");
-        }
+        strcpy(buf, "");
     }
 
-    printf("There are %d packets\n", total_frag);
+    //close the socket
+    close(sd);
 
-    return 0;
+    free(reply);
+    
+    return (0);
 }
