b3bacb4a16378d3437b33752fe77502cb9eaa5b2
Ramakrishna Natarajan <nkrama99@gmail.com>
2022-11-09 15:16:45 -0500

    server open tcp connection on specified port

--- TextConferencingLab/section1/server.c
+++ TextConferencingLab/section1/server.c
@@ -1,5 +1,18 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define BACKLOG 10	 // how many pending connections queue will hold
+#define PORT "3490"  // the port users will be connecting to
 
 int main(int argc, char **argv)
 {
@@ -11,6 +24,65 @@ int main(int argc, char **argv)
 
     int server_port_number = atoi(argv[1]);
     printf("Initiating Text Conferencing Server on %d\n", server_port_number);
+
+    int sockfd, new_fd; // listen on sock_fd, new connection on new_fd
+    int rv;
+    int yes = 1;
+    struct addrinfo hints, *servinfo, *p;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and bind to the first we can
+    for (p = servinfo; p != NULL; p = p->ai_next)
+    {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                             p->ai_protocol)) == -1)
+        {
+            perror("server: socket");
+            continue;
+        }
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                       sizeof(int)) == -1)
+        {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+        {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if (p == NULL)
+    {
+        fprintf(stderr, "server: failed to bind\n");
+        exit(1);
+    }
+
+    if (listen(sockfd, BACKLOG) == -1)
+    {
+        perror("listen");
+        exit(1);
+    }
+
+    printf("server: waiting for connections...\n");
 
     exit(0);
 }
